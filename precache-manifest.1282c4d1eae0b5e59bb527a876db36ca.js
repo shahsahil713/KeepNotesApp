d123c3f8abba62bc2a908eb5ffed08b6e0c72e7d
Shah Sahil <shahsahil713@gmail.com>
2020-06-25 23:05:49 +0530

    Updates

---
+++ precache-manifest.1282c4d1eae0b5e59bb527a876db36ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "857e595c04b8104d3cb63d09a9a1232e",
+    "url": "/KeepNotesApp/index.html"
+  },
+  {
+    "revision": "9fddceba38e1a48ebc86",
+    "url": "/KeepNotesApp/static/css/main.04d8d737.chunk.css"
+  },
+  {
+    "revision": "297ba39487dd78a28c28",
+    "url": "/KeepNotesApp/static/js/2.b853c71e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/KeepNotesApp/static/js/2.b853c71e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fddceba38e1a48ebc86",
+    "url": "/KeepNotesApp/static/js/main.32215a50.chunk.js"
+  },
+  {
+    "revision": "a80155fed33f2ff77c48",
+    "url": "/KeepNotesApp/static/js/runtime-main.e6c0a212.js"
+  }
+]);
